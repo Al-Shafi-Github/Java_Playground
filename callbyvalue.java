c5f9c6198dcb3d9890fab4f9660d6d997ddb1276
Abdullah Al Shafi <68460013+Al-Shafi-Github@users.noreply.github.com>
2023-07-27 22:10:14 +0600

    trying call by value

---
+++ callbyvalue.java
@@ -0,0 +1,12 @@
+public class callbyvalue {
+
+	public static void main(String[] args) {
+		callvaluetest ob = new callvaluetest();
+		int a=15,b=20;
+		ob.meth(a,b);
+		System.out.println(a+ " " + b);
+	}	
+	
+
+}
+
