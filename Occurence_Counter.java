a07d360436aebed6c4ada1946eea863eb9b85bed
Abdullah Al Shafi <68460013+Al-Shafi-Github@users.noreply.github.com>
2023-07-27 21:47:42 +0600

    uploading Optimzed Code

---
+++ Occurence_Counter.java
@@ -0,0 +1,37 @@
+import java.util.Scanner;
+public class Occurence_Counter {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		 Scanner in = new Scanner(System.in);
+
+	        int[] values = new int[100];
+	        int input;
+	        int count = 0;
+	        System.out.print("Enter the integers between 1 and 100: ");
+	        do
+	        {
+	            input = in.nextInt();
+	            values[count] = input;
+	            count += 1;
+	        }
+	        while (input != 0);
+	        in.close();
+
+	        countOccurence(values);
+	    }
+
+	    public static void countOccurence(int[] list) {
+	        for (int i = 1; i <= 100; i++) {
+	            int count = 0;
+	            for (int j = 0; j < list.length - 1; j++) {
+	                if (list[j] == i)
+	                    count += 1;
+	            }
+	            if (count != 0)
+	                System.out.printf("%d occurs %d %s%n", 
+	                    i, count, count > 1 ? "times" : "time");
+	        } 
+	   }
+
+}
