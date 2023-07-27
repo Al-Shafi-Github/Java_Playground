8a5ab21a09def179bd24e2aaa798129bdceac81e
Abdullah Al Shafi <68460013+Al-Shafi-Github@users.noreply.github.com>
2023-07-27 21:52:50 +0600

    Prime Number checked

---
+++ Prime_number.java
@@ -0,0 +1,41 @@
+import java.util.Scanner;
+public class Prime_number {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+	    	 final int NUMBER_OF_PRIMES = 50;
+	        int[] primeNumbers = new int[NUMBER_OF_PRIMES];
+	        int index = 0;
+
+	        for (int i = 2; index < NUMBER_OF_PRIMES; i++) {
+
+	            if (isPrime(i)) {
+	                primeNumbers[index++] = i;
+	            }
+	        }
+
+	       printArray(primeNumbers, 10);
+
+	    }
+
+	    public static boolean isPrime(int number) 
+	    {
+
+	        for (int i = 2; i <= Math.sqrt(number); i++) {
+	            if (number % i == 0) 
+	            	return false;
+
+	        }
+
+	        return true;
+	    }
+
+	    public static void printArray(int[] array, int numberPerLine) {
+
+	        for (int i = 0; i < array.length; i++) {
+
+	            System.out.printf("%10d ", array[i]);
+	            if ((i + 1) % numberPerLine == 0) System.out.println("");
+	        }
+	    }
+	}
