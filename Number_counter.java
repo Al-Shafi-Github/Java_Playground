fdb9e32e0fae0119b4f3b2b4db9db38f6a6fb61e
Abdullah Al Shafi <68460013+Al-Shafi-Github@users.noreply.github.com>
2023-07-27 21:54:11 +0600

    counting numbers

---
+++ Number_counter.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+public class Number_counter {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int[] counts = new int[10]; // Array of ten integers
+
+		// Store the counts of 100 random numbers
+		for (int i = 1; i <= 100; i++) {
+			counts[(int)(Math.random() * 10)]++;
+		}
+
+		// Display the results
+		System.out.println("Count for each number between 0 and 9:");
+		for (int i = 0; i < counts.length; i++) {
+			System.out.println(i + "s: " + counts[i]);
+		}
+	}
+
+}
